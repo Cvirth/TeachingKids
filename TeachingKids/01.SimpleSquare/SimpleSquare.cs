4c3c1790738b7a0ce787079caf4ea6356cb40553
Cintia Virth <virthcintia@gmail.com>
2017-10-31 17:32:31 +0100

    Sketches reversed

--- TeachingKids/01.SimpleSquare/SimpleSquare.cs
+++ TeachingKids/01.SimpleSquare/SimpleSquare.cs
@@ -6,61 +6,21 @@ namespace SimpleSquare
 	{
 		public static void Main2()
 		{
-            var speed = 10;
-
             Tortoise.Show();
-
-            //Tortoise.SetSpeed(speed);
-
-            var actualSpeed = Tortoise.GetSpeed();
-            Tortoise.SetSpeed(actualSpeed + 1);
-
-
-            speed = 5;
-
-            //Tortoise.SetSpeed(speed);
-            
-            //Tortoise.SetSpeed(10);
+            Tortoise.SetSpeed(10);
             Tortoise.InstantSpeed(goFast: false);
             // true false
             for (int i = 0; i < 4; i++)
             {
-                Tortoise.SetPenColor("#FF00FF");
+                Tortoise.SetPenColor(color: "blue");
                 Tortoise.Move(lengthInPixels: 50);
-                Tortoise.Turn(90);
+                Tortoise.Turn(degrees: 90);
             }
-
-            
 
             Fv1(true, "aaa");
 		}
 
         public static void Fv1(bool goFast, string aa2)
-        {
-            //Tortoise.SetAngle
-            Paint.SetSpeed(10);
-            //Paint.InstantSpeed(,);
-        }
-    }
-
-    public class Paint
-    {
-        public static void Show()
-        {
-
-        }
-        public static void SetSpeed(int number)
-        {
-
-        }
-
-        /// <summary>
-        /// Set the speed to be instant or not.
-        /// If false, then the Speed ...
-        /// </summary>
-        /// <param name="goFast">aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</param>
-        /// <param name="second">bbbbbbbbbbbbbbbbbbbbbbbbbbb</param>
-        public static void InstantSpeed(bool goFast, bool second)
         {
 
         }
